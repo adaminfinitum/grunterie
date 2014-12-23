e74d8b004c6cf0ba147a9360af989c99b24ce18a
Adam Powell <adam@adaminfinitum.com>
2014-12-23 14:49:44 -0500

    improved functionality of HTML sitemap

--- page-sitemap.php
+++ page-sitemap.php
@@ -1,29 +1,39 @@
 <?php
-/*
-Template Name: HTML Sitemap Page
-*/
-get_header(); ?>
+	/*
+	Template Name: HTML Sitemap Page
+	*/
+	get_header();
+?>
 
-<!-- Row for main content area -->
+	<!-- Row for main content area -->
 	<div class="small-12 large-8 columns" id="content" role="main" itemprop="mainContentOfPage">
 
-	<?php /* Start loop */ ?>
-	<?php while (have_posts()) : the_post(); ?>
+	<?php
+		/* Start loop */
+		while (have_posts()) : the_post();
+	?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
 				<h1 class="entry-title" itemprop="headline"><?php the_title(); ?></h1>
 			</header>
 			<div class="entry-content">
 				<?php the_content(); ?>
-				<?php get_template_part('/partials/sitemap'); ?>
 			</div>
 			<footer>
 				<?php wp_link_pages(array('before' => '<nav id="page-nav"><p>' . __('Pages:', 'reverie'), 'after' => '</p></nav>' )); ?>
 			</footer>
 		</article>
-	<?php endwhile; // End the loop ?>
+
+	<?php
+		endwhile;
+		/* End loop */
+
+		get_template_part('/partials/sitemap');
+	?>
 
 	</div>
-	<?php get_sidebar(); ?>
 
-<?php get_footer(); ?>
+<?php
+	get_sidebar();
+	get_footer();
+?>
